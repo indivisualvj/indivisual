ccc4df2f6d8aacfb1e79bbb090827bf13064d376
Lenjamin Butze <benjamin.lutze@gmail.com>
2019-02-11 21:02:18 +0100

    materials point+line bugfixes

--- app/js/plugins/mesh_material/line.js
+++ app/js/plugins/mesh_material/line.js
@@ -2,6 +2,7 @@ HC.plugins.mesh_material.line = _class(false, HC.MeshMaterialPlugin, {
     name: 'line (no transform)',
     apply: function (geometry) {
         var material = new THREE.LineBasicMaterial();
+        // material.lights = true;
         var g = new THREE.EdgesGeometry(geometry);
         var mesh = new THREE.LineSegments(g, material);
         g.userData.geometry = geometry;
--- app/js/plugins/mesh_material/point.js
+++ app/js/plugins/mesh_material/point.js
@@ -2,7 +2,7 @@ HC.plugins.mesh_material.point = _class(false, HC.MeshMaterialPlugin, {
     name: 'points (no transform -> edges only)',
     apply: function (geometry, index) {
         var material = new THREE.PointsMaterial();
-        material.sizeAttenuation = false;
+        // material.lights = true;
         var g = new THREE.EdgesGeometry(geometry);
         var mesh = new THREE.Points(g, material);
         g.userData.geometry = geometry;
@@ -14,7 +14,7 @@ HC.plugins.mesh_material.transformablepoints = _class(false, HC.MeshMaterialPlug
     name: 'points (transform)',
     apply: function (geometry, index) {
         var material = new THREE.PointsMaterial();
-        material.sizeAttenuation = false;
+        // material.lights = true;
         var mesh = new THREE.Points(geometry, material);
         return mesh;
     }

34c6966fd2dd48c8ce2e240445fcfd2f9350f9bc
Lenjamin Butze <benjamin.lutze@gmail.com>
2019-02-23 12:19:44 +0100

    hive bugfixes

--- app/js/plugins/background_mode/sphere.js
+++ app/js/plugins/background_mode/sphere.js
@@ -13,6 +13,20 @@ HC.plugins.background_mode.sphere = _class(false, HC.BackgroundModePlugin, {
 
             this.layer.setBackground(mesh);
 
+            var file;
+            if (file = assetman.getImage(this.settings.background_input)) {
+                new THREE.TextureLoader().load(filePath(IMAGE_DIR, file), function (tex) {
+
+                    tex.wrapS = THREE.RepeatWrapping;
+                    tex.repeat.x = - 1;
+                    mat.map = tex;
+                    // mat.color.setHSL(0, 0, 1);
+                    mat.emissiveMap = tex;
+                    // tex.mapping = THREE.SphericalReflectionMapping;
+                    // tex.wrapS = THREE.MirroredRepeatWrapping;
+                    mat.needsUpdate = true;
+                });
+            }
         }
     }
 });

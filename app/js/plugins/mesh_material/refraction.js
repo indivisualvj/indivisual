5e2c16e0976833052c88476c6b9e27db43d02f3f
Lenjamin Butze <benjamin.lutze@gmail.com>
2019-03-08 23:41:25 +0100

    blue marbles

--- app/js/plugins/mesh_material/refraction.js
+++ app/js/plugins/mesh_material/refraction.js
@@ -1,31 +1,43 @@
-{
-    HC.plugins.mesh_material.refraction = class Plugin extends HC.MeshMaterialPlugin {
-        static index = 90;
-
-        apply(geometry, index) {
-
-            // todo refractionRatio==shinyness?
-            var material = new THREE.MeshStandardMaterial({color: 0xffffff, envMap: this.layer.three.scene.background, refractionRatio: 0.95});
-            material.envMap.mapping = THREE.CubeRefractionMapping;
-            let mesh = new THREE.Mesh(geometry, material);
-
-            let inst = this;
-            listener.register('animation.updateSetting', this.id(index), function (data) {
-                if (inst.layer.isVisible()) {
-                    switch (data.item) {
-                        case 'background_mode':
-                        case 'background_input':
-                            material.envMap = inst.layer.three.scene.background;
-                            break;
-                    }
-                }
-            });
-
-            return mesh;
-        }
-
-        reset() {
-
-        }
-    }
-}
+// {
+//     HC.plugins.mesh_material.refraction = class Plugin extends HC.MeshMaterialPlugin {
+//         static index = 90;
+//
+//         apply(geometry, index) {
+//
+//             // todo refractionRatio==shinyness?
+//             var material = new THREE.MeshBasicMaterial({
+//                 envMap: new THREE.Texture(),
+//                 color: 0xffffff,
+//                 refractionRatio: 0.95
+//             });
+//
+//             let plug = this.layer.getBackgroundModePlugin('texture');
+//             if (plug.img) {
+//                 material.envMap = plug.img;
+//                 material.envMap.generateMipmaps = true;
+//                 material.envMap.mapping = THREE.CubeRefractionMapping;
+//                 material.needsUpdate = true;
+//             }
+//
+//             let mesh = new THREE.Mesh(geometry, material);
+//
+//             let inst = this;
+//             listener.register('animation.updateSetting', this.id(index), function (data) {
+//                 if (inst.layer.isVisible()) {
+//                     switch (data.item) {
+//                         case 'background_mode':
+//                         case 'background_input':
+//                             inst.apply(geometry, index);
+//                             break;
+//                     }
+//                 }
+//             });
+//
+//             return mesh;
+//         }
+//
+//         reset() {
+//
+//         }
+//     }
+// }

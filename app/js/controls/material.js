b9ceeaa0f32388ef88cb56ae97f3c7bb6380f1af
Benjamin Lutze <account@bennson.de>
2020-01-22 20:56:10 +0100

    todos

--- app/js/controls/material.js
+++ app/js/controls/material.js
@@ -17,7 +17,7 @@
             material_blendequation: 'AddEquation',
             material_blendsrc: 'SrcAlphaFactor',
             material_blenddst: 'OneMinusSrcAlphaFactor',
-            material_side: 2, // fixme not reset on resetFolder (datatype issue?!)
+            material_side: 2, // fixme is isModified gives allwas false becaus of Select cmp in guify onchange gives strings always
             material_shininess: 2,
             material_volume: 1,
             material_roughness: 0.5,

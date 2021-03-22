c824b71bd0fd47ab5409a0432694b08a0c9ec7e7
indivisualvj <account@bennson.de>
2021-03-22 21:13:33 +0100

    omg

---
+++ app/js/plugins/mesh_material/shader/craziness.js
@@ -0,0 +1,48 @@
+// source: https://www.shadertoy.com/view/wdjSRc
+// FORKED FROM Ether by nimitz (twitter: @stormoid)
+// https://www.shadertoy.com/view/MsjSW3
+{
+    HC.plugins.mesh_material.craziness = class Plugin extends HC.MeshShaderMaterialPlugin {
+
+        shader = {
+            uniforms: {
+                uTime: {type: 'f', value: 1.0},
+                // resolution: {type: "v2", value: new THREE.Vector2(800, 600)}
+            },
+            fragmentShader: `
+            varying vec2 vUv;
+            vec2 resolution = vec2(1.0);
+            uniform float uTime;
+            
+            #define t uTime/4.0
+            mat2 m(float a){float c=cos(a), s=sin(a);return mat2(c,-s,s,c);}
+            float map(vec3 p){
+                p.xz*= m(t*0.4);p.xy*= m(t*0.3);
+                vec3 q = p*2.+t;
+                return length(p+vec3(sin(t*0.7)))*log(length(p)+1.) + sin(q.x+sin(q.z+sin(q.y)))*5.5 - 1.;
+            }
+            
+            void mainImage( out vec4 fragColor, in vec2 fragCoord ){	
+                vec2 p = fragCoord.xy/resolution.y - vec2(.9,.5);
+                vec3 cl = vec3(0.);
+                float d = .9;
+                for(int i=0; i<=5; i++)	{
+                    vec3 p = vec3(0,0,5.) + normalize(vec3(p, -1.))*d;
+                    float rz = map(p);
+                    float f =  clamp((rz - map(p+.1))*0.5, -.1, 1. );
+                    vec3 l = vec3(0.1,0.3,.4) + vec3(5., 2.5, 3.)*f;
+                    cl = cl*l + (1.-smoothstep(0., 2.5, rz))*.7*l;
+                    d += min(rz, 1.);
+                }
+                fragColor = vec4(cl, 1.);
+            }
+            
+            void main() {
+                mainImage(gl_FragColor, vUv);
+            }
+            `
+            ,
+            vertexShader: "varying vec2 vUv;void main(){vUv = uv;vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );gl_Position = projectionMatrix * mvPosition;}"
+        }
+    }
+}

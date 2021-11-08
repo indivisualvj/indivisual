032dcc6fd902e63d2e3e48d271efeb525d4ed203
indivisualvj <account@bennson.de>
2021-11-08 13:37:25 +0100

    mask clip error fix no

--- app/js/display/mask.js
+++ app/js/display/mask.js
@@ -130,7 +130,6 @@
             let clipPath = this.points.join(', ');
             clipPath = 'polygon(' + clipPath.replace(/([^,]+), ([^,]+)/gi, '$1px $2px') + ')';
             this.canvas.style.clipPath = clipPath;
-            this.canvas.style.transform = 'transform:scaleZ(1)';
         }
     }
 }

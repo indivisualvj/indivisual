e4e6f40e37d92c9c156409657c4921b357090f59
indivisualvj <account@bennson.de>
2021-11-09 16:00:34 +0100

    remove todos

---
+++ app/js/plugins/shaders/edge-detection.js
@@ -0,0 +1,18 @@
+// {
+//     HC.plugins.shaders.edge_detection = class Plugin extends HC.ShaderPlugin {
+//         static index = 144;
+//         static name = 'edge-detection';
+//         create() {
+//             if (!this.pass) {
+//                 this.pass = new THREE.ShaderPass(THREE.EdgeShader);
+//             }
+//
+//             return this.pass;
+//         }
+//
+//         static settings = {
+//             apply: false,
+//             random: false,
+//         };
+//     };
+// }

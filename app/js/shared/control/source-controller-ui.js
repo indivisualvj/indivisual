03e78a7f2907001185d5f481d72e8c3a58f2bd39
visual.indi <ennyennson@gmail.com>
2021-11-13 21:49:36 +0100

    Dev (#43)
    * mask clip error fix no
    
    * mess around with fps drops
    
    * mess around with fps drops
    
    * replace all == \d with === \d
    
    * presets
    
    * ui cleanup
    
    * optimize loading paths/names
    
    * use three from npm
    
    * exclude yarn-error
    
    * ...
    
    * use js-yaml from npm
    
    * remove todos
    
    * better sequence controller
    
    * fix error if another guify version in use
    
    * som fixes. maybe
    
    * ==2===
    
    * cleanup
    
    * remove todos
    
    * add clips in process
    
    * alles geil gemacht
    
    * som fixes. maybe
    
    * Shuffleable (#42)
    
    * som fixmes
    
    * fix openByProp
    
    * fix tutorial object string
    
    * the big shuffleable refactoring
    
    * audio-fix, server show local-ip
    
    * audio-fix, server show local-ip
    
    * audio-fix
    
    * server error handling and loggin
    
    * no require process
    
    * ui updates
    
    * ui updates
    
    * ui updates
    
    * fix shapetastic.js
    
    * remove LAAAGGGGGG
    
    * sync reactiontime to 1/2 fps
    
    * make load/reset presets work
    
    * accessibility enhancements
    
    * fix f2b

--- app/js/shared/control/source-controller-ui.js
+++ app/js/shared/control/source-controller-ui.js
@@ -15,7 +15,29 @@
          * @param that
          */
         onChange(value, that) {
-            messaging.program.updateSource(that.getProperty(), value, true, true, false);
+            messaging.program.updateSource(that.getProperty(), value, false, true, false);
+        }
+
+
+        /**
+         * @param hook {Function|null}
+         */
+        addControllers(hook) {
+
+            let folders = {};
+            for(let key in this.controlSet.settings) {
+                let ctrl = this.addController(key);
+                if (ctrl) {
+                    folders[ctrl.getParent().getLabel()] = ctrl.getParent();
+                }
+            }
+
+            for (let k in folders) {
+                this._finishFolder(folders[k]);
+                if (hook) {
+                    hook(folders[k]);
+                }
+            }
         }
 
 

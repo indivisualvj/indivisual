018ba686434ec52500ec5ff9e8e46bbc6046b25c
visual.indi <account@bennson.de>
2021-11-19 23:37:19 +0100

    fix all shader props are now getting removed

--- app/js/controller.js
+++ app/js/controller.js
@@ -573,7 +573,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if (layer === undefined) {
                 layer = this.config.ControlSettings.layer;
             }
-            // fixme: if passes are loaded via CTRL+preset, passes added cannot be removed completely.
+
             let passes = this.settingsManager.get(layer, 'passes');
             let pass = {};
             pass[ctrl.name] = ctrl.getShader();

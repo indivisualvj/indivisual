229a39ee86b33bda428494846f83ba0679d56dd7
visual.indi <account@bennson.de>
2021-11-22 20:33:24 +0100

    nicer dicer

--- app/js/plugins/offset_mode/sphere.js
+++ app/js/plugins/offset_mode/sphere.js
@@ -2,44 +2,18 @@
     HC.plugins.offset_mode.sphere = class Plugin extends HC.OffsetModePlugin {
         static name = 'sphere';
 
-        apply(shape, revert) {
+        apply(shape) {
             let layer = this.layer;
+            let pos = layer.patternCenterVector(true);
 
-            let m = 1;
-            let pos = shape.position();
-            let dist = Math.max(pos.distanceTo(layer.patternCenterVector(true)), 1);
-            let _dist = layer.resolution().length();
-
-            if (revert) {
-                let diff = (dist - _dist);
-                let f = diff / _dist;
-                m = 1 - f;
-
-            } else {
-                m = dist / _dist;
-            }
-
-            if (isNaN(m)) {
-                m = 1;
-            }
-
-            m *= -layer.shapeSize(.5);
-
+            let a = shape.position().distanceTo(pos);
+            let m = a * layer.shapeSize(.01);
             let x = this.settings.offset_x * m;
             let y = this.settings.offset_y * m;
             let z = this.settings.offset_z * m;
 
             shape.offset(x, y, z);
-        }
-    }
-}
-{
-    HC.plugins.offset_mode.desphere = class Plugin extends HC.OffsetModePlugin {
-        static name = 'desphere';
 
-        apply(shape) {
-            let layer = this.layer;
-            layer.getOffsetModePlugin('sphere').apply(shape, true);
         }
     }
 }

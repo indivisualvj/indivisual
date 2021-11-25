2eb193afb8d870795d993321b1791c344fb2bd60
visual.indi <account@bennson.de>
2021-11-25 11:37:46 +0100

    precision

--- app/js/plugins/mesh_material/_plugin.js
+++ app/js/plugins/mesh_material/_plugin.js
@@ -8,44 +8,7 @@ HC.plugins.mesh_material = HC.plugins.mesh_material || {};
         material;
 
         before(geometry) {
-
-            // if (geometry.mergeVertices) {
-            //     geometry.mergeVertices();
-            // }
-            // if (geometry.computeFaceNormals) {
-            //     geometry.computeFaceNormals();
-            // }
-            // if (geometry.computeFlatVertexNormals) {
-            //     geometry.computeFlatVertexNormals();
-            // }
-            // if (geometry.computeVertexNormals) {
-            //     geometry.computeVertexNormals();
-            // }
-            // if (geometry.computeMorphNormals) {
-            //     geometry.computeMorphNormals();
-            // }
-
-            // geometry.elementsNeedUpdate = true;
-            // geometry.verticesNeedUpdate = true;
-            // geometry.uvsNeedUpdate = true;
-            // geometry.normalsNeedUpdate = true;
-            // geometry.colorsNeedUpdate = true;
-
-            let geo;
-            if (geometry.isGeometry) {
-                geo = new THREE.BufferGeometry().fromGeometry(geometry);
-
-            } else {
-                geo = geometry;
-            }
-            // else {
-            //     geo = new THREE.BufferGeometry().setFromObject(geometry);
-            // }
-
-            if (geo) {
-                geo.userData.geometry = geometry;
-                geometry = geo;
-            }
+            // do nothing until needed
 
             return geometry;
         }

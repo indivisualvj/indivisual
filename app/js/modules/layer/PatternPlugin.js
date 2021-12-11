66065d4bd2aad533971e27bc10a8ca6f5392af44
visual.indi <account@bennson.de>
2021-12-11 16:32:54 +0100

    // fixme: no space after select fields (shaders, session) ?

--- app/js/modules/layer/PatternPlugin.js
+++ app/js/modules/layer/PatternPlugin.js
@@ -8,7 +8,7 @@ class PatternPlugin extends AnimationPlugin
 
     setControlSets(controlSets) {
         super.setControlSets(controlSets);
-        // make all such plugins make use of corresponding controlset only
+        // make all such plugins use of corresponding controlset only
         this.settings = controlSets.pattern.properties;
     }
 
